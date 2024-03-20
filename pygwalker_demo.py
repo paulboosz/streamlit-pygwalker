24fd2f7b6c4d71b7184dcdba0aa53ee46a023045
paulboosz <paulboosz@gmail.com>
2024-03-20 15:44:58 +0100

    update

--- pygwalker_demo.py
+++ pygwalker_demo.py
@@ -4,23 +4,25 @@ import streamlit.components.v1 as components
 import streamlit as st
 
 # Adjust the width of the Streamlit page
-st.set_page_config(page_title="Ecobalyse Suivi des modifs", layout="wide")
+st.set_page_config(page_title="Ecobalyse - Suivi des modifs", layout="wide")
 
 # Add Title
-st.title("Ecobalyse Suivi des modifs")
+st.title("Ecobalyse - Suivi des modifs")
 
 # Import your data
 df = pd.read_csv("textile_consolidated.csv")
 
+
 def load_config(file_path):
-    with open(file_path, 'r') as config_file:
+    with open(file_path, "r") as config_file:
         config_str = config_file.read()
     return config_str
-  
-config = load_config('config.json')
+
+
+config = load_config("config.json")
 
 # Generate the HTML using Pygwalker
-pyg_html = pyg.to_html(df, spec=config)
+pyg_html = pyg.to_html(df, spec=config, dark="light")
 
 # Embed the HTML into the Streamlit app
 components.html(pyg_html, height=1000, scrolling=True)
