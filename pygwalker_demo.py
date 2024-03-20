8dc2f28f5dec2d890bf8110d544182ec9eb8a602
paulboosz <paulboosz@gmail.com>
2024-03-20 10:43:06 +0100

    Update pygwalker_demo.py

--- pygwalker_demo.py
+++ pygwalker_demo.py
@@ -4,10 +4,10 @@ import streamlit.components.v1 as components
 import streamlit as st
 
 # Adjust the width of the Streamlit page
-st.set_page_config(page_title="Use Pygwalker In Streamlit", layout="wide")
+st.set_page_config(page_title="Ecobalyse Suivi des modifs", layout="wide")
 
 # Add Title
-st.title("Use Pygwalker In Streamlit")
+st.title("Ecobalyse Suivi des modifs")
 
 # Import your data
 df = pd.read_csv("textile_consolidated.csv")
